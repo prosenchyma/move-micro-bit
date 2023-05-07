2779365672769bd32f0b70ed760920d65a73784b
prosenchyma <130674956+prosenchyma@users.noreply.github.com>
2023-05-07 12:19:08 +0200

    Update main.py
    Commented out unnecessary code

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def on_button_pressed_a():
         if randomnumber == 1:
             forwardcounter += 1
             kitronik_servo_lite.drive_forwards(100)
-            serial.write_line("forward" + forwardcounter)
+            # serial.write_line("forward" + forwardcounter) for testing
         if randomnumber == 2:
             kitronik_servo_lite.drive_backwards(100)
-            serial.write_line("backward")
+            # serial.write_line("backward") for testing
         basic.pause(500) 
 input.on_button_pressed(Button.A, on_button_pressed_a)
